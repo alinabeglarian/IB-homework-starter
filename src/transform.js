7534607578fb0fc22d2a2afd8518781d3f32ccf5
alinabeglarian <alinabeglarian@gmail.com>
2019-06-08 21:43:06 +0200

    final version

--- src/transform.js
+++ src/transform.js
@@ -1,4 +1,4 @@
-module.exports.groupAdultsByAgeRange = function (people) {
+module.exports.groupAdultsByAgeRange = (people) => { 
     const initialAccumulator = {
         group1: [],
         group2: [],
@@ -6,65 +6,26 @@ module.exports.groupAdultsByAgeRange = function (people) {
         group4: [],
         group5: []
     }
+    
     return people
-
         .filter((person) => person.age > 17)
-        .reduce((groups, people) => {
-            if (people.age < 21) {
+        .reduce((groups, person) => {
+            if (person.age < 21) {
                 groups.group1.push(person)
                 return groups
             }
-            if (people.age < 31) {
+            if (person.age < 31) {
                 groups.group2.push(person)
                 return groups
             }
-            if (people.age < 41) {
+            if (person.age < 41) {
                 groups.group3.push(person)
                 return groups
             }
-            if (people.age < 51) {
+            if (person.age < 51) {
                 groups.group4.push(person)
             }
                 groups.group5.push(person)
                 return groups
         }, initialAccumulator)
 }
-        
-
-// const initialAccumulator = {
-//     '20 and younger': [],
-//     '21-30': [],
-//     '31-40': [],
-//     '41-50': [],
-//     '51 and older': []
-// }
-
-// - `20 and younger`
-// - `21-30`
-// - `31-40`
-// - `41-50`
-// - `51 and older`
-
-
-// onst categorizePokemonsByRarity = (pokemons) => {
-//     const RARE_SPAWN_CHANCE = 0.1
-//     const LEGENDARY_SPAWN_CHANCE = 0.01
-    
-//     const initialAccumulator = {
-//         common: [],
-//         rare: [],
-//         legendary: []
-//     }
-
-//     return pokemons.reduce((categories, pokemon) => {
-//         if (pokemon.spawn_chance > RARE_SPAWN_CHANCE) {
-//             categories.common.push(pokemon)
-//             return categories
-//         }
-//         if (pokemon.spawn_chance > LEGENDARY_SPAWN_CHANCE) {
-//             categories.rare.push(pokemon)
-//             return categories
-//         }
-//         categories.legendary.push(pokemon)
-//         return categories
-//     }, initialAccumulator)
